9b58998f43289b76b59a493365a60b24becd0c14
dredknight <yordank@office.corp>
2020-10-12 15:29:45 +0300

    up

--- VMware/Powercli/VMsbyTag.ps1
+++ VMware/Powercli/VMsbyTag.ps1
@@ -1,28 +1,27 @@
 [CmdletBinding()]
 Param(
  [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
- [string]$v, ##vcenter FQDN
+ [string]$v, 		## vcenter FQDN
  [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
- [string]$u, ##username
+ [string]$u,		## username
  [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
- [string]$p, ##password
+ [string]$p, 		## password
  [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
- [string]$tag,
- [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
- [string]$output,
+ [string]$tag, 		## filter got get VMs by
  [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
- [string]$sortBy,
+ [string]$output="test", 	## file to save report output
  [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
- [string]$colour='#e6f2ff'
+ [string]$sortBy,   ## sort output
+ [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+ [string]$colour='#e6f2ff', ## table row colour
+ [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+ [string]$title=$v + " - " + $tag, ## report title
+ [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+ [string]$klas="table1" ## class to separate CSS styling from other HTML fragments
 )
 
-if($tag -eq "")
-{
-	$title = $v + " - " + "no tag"
-} else
-{
-	$title = $v + " - " + $tag
-}
+##$title = $v + " - " + "high"
+write-host "$title"
 
 function header{
  $style = @"
@@ -31,7 +30,7 @@ function header{
  font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
  }
  
- table{
+ table.$klas{
   border-collapse: collapse;
   border: none;
   font: 10pt Verdana, Geneva, Arial, Helvetica, sans-serif;
@@ -39,14 +38,14 @@ function header{
   margin-bottom: 10px;
  }
  
- table td{
+ table.$klas td{
   font-size: 12px;
   padding-left: 0px;
   padding-right: 20px;
   text-align: left;
  }
  
- table th{
+ table.$klas th{
   font-size: 12px;
   font-weight: bold;
   padding-left: 0px;
@@ -62,18 +61,18 @@ function header{
   margin-left: 10px; font-size: 12px;
  }
  
- table.list{
+ table.$klas.list{
   float: left;
  }
  
- table tr:nth-child(even){background: $colour;} 
- table tr:nth-child(odd) {background: #FFFFFF;}
+ table.$klas tr:nth-child(even){background: $colour;} 
+ table.$klas tr:nth-child(odd) {background: #FFFFFF;}
 
  div.column {width: 320px; float: left;}
  div.first {padding-right: 20px; border-right: 1px grey solid;}
  div.second {margin-left: 30px;}
 
- table{
+ table.$klas{
   margin-left: 10px;
  }
  –>
@@ -82,7 +81,65 @@ function header{
 
  return [string] $style
  }
+
+function header_backup{
+ $style = @"
+ <style>
+ body{
+ font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
+ }
  
+ table.$klas{
+  border-collapse: collapse;
+  border: none;
+  font: 10pt Verdana, Geneva, Arial, Helvetica, sans-serif;
+  color: black;
+  margin-bottom: 10px;
+ }
+ 
+ table.$klas td{
+  font-size: 12px;
+  padding-left: 0px;
+  padding-right: 20px;
+  text-align: left;
+ }
+ 
+ table.$klas th{
+  font-size: 12px;
+  font-weight: bold;
+  padding-left: 0px;
+  padding-right: 20px;
+  text-align: left;
+ }
+ 
+ h2{
+  clear: both; font-size: 130%;color:#00134d;
+ }
+ 
+ p{
+  margin-left: 10px; font-size: 12px;
+ }
+ 
+ table.$klas.list{
+  float: left;
+ }
+ 
+ table.$klas tr:nth-child(even){background: $colour;} 
+ table.$klas tr:nth-child(odd) {background: #FFFFFF;}
+
+ div.column {width: 320px; float: left;}
+ div.first {padding-right: 20px; border-right: 1px grey solid;}
+ div.second {margin-left: 30px;}
+
+ table.$klas{
+  margin-left: 10px;
+ }
+ –>
+ </style>
+"@
+
+ return [string] $style
+ }
  
 function login($vCenter, $user, $password) {
 	connect-viserver $vCenter -user $user -password $password -ErrorAction SilentlyContinue
@@ -148,20 +205,30 @@ function varsAll {
 }
 
 function varsBackup {
+	
+	if (($vm |Get-CDDrive).IsoPath)	{ $iso = "yes" } else { $iso = "" }
 	[PSCustomObject]@{
 		"Name" = $vm.Name
 		"Host" = $vm.VMHost.Name
 		"Version" = $vm.ExtensionData.Config.Version
 		"OS" = $vm.ExtensionData.Config.GuestFullName
-		"attached ISO" = ($vm |Get-CDDrive).IsoPath
+		"ISO?" = $iso
 	}
 }
-
 
 #$ips=$vm.guest.net.ipaddress
 #if ($ips.count -gt 1) {$ips=$vm.guest.net.ipaddress[0] + " " + $vm.guest.net.ipaddress[1]}
 
+function logout($vCenter) {
+#Disconnect from vCenter or ESXi
+  Disconnect-VIServer -Confirm:$False -Server $vCenter -ErrorAction Stop
+}
 
 login $v $u $p
-VMsbyTag |Sort-Object -Property Name| ConvertTo-Html -Head $(header) -PreContent $title | Add-Content c:\TEMP\$output
-
+$frag = VMsbyTag |Sort-Object -Property Name| ConvertTo-Html -Head $(header_backup) 
+$frag = $frag -replace '<table>',"<table class=`"$klas`">"
+## Debug
+#$frag = VMsbyTag |Sort-Object -Property Name| ConvertTo-Html -Head $(header_backup) -PreContent $title
+#$frag = $frag -replace '<table>',"<table class=`"$klas`">" | Add-Content c:\TEMP\$output
+logout($v)
+return $frag
