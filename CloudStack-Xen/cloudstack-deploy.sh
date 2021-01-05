c15b55c78e20cae1c8247bba844791e9bff475d8
dredknight <yordank@office.corp>
2021-01-05 17:03:02 +0200

    up

--- CloudStack-Xen/cloudstack-deploy.sh
+++ CloudStack-Xen/cloudstack-deploy.sh
@@ -5,48 +5,13 @@
 
 ## Defining vars
 WORK_DIR="/tmp"
-FQDN="cloudstack.domain.int"		## host FQDN
-ntp_1="0.0.0.0"					## first ntp server to sync
-ntp_2="0.0.0.0"					## second ntp server to sync
 db_root_password="root"			## mariadb root password
-db_cloud_password="cloud"         ## password for mariadb cloudstack user cloud
+db_cloud_password="cloud"       ## password for mariadb cloudstack user cloud
 
 ## Installing prerequisites
 printf "\n############### PREREQUISITES ################\n"
 yum install expect epel-release -y -q
 yum repolist
-yum install mysql-connector-python -y -q
-
-## Disable SELINUX
-printf "\n############### SELINUX CONFIG ################\n"
-sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
-setenforce permissive
-sestatus
-
-## Disable firewalld
-#systemctl disable firewalld 2>/dev/null
-#systemctl stop firewalld 2>/dev/null
-printf "\n############### FIREWALL CONFIG ################\n"
-firewall-cmd --zone=public --add-port=8080/tcp --permanent
-firewall-cmd reload
-printf "port 8080 tcp enabled"
-systemctl status firewalld
-
-## Configure FQDN
-printf "\n############### HOSTNAME ################\n"
-printf $FQDN > /etc/hostname
-hostname --fqdn
-
-## Configure ntp
-printf "\n############### NTP CONFIG ################\n"
-yum install ntp -y -q
-sytemctl enable ntpd 2>/dev/null
-sed -i "s/0.centos.pool.ntp.org/$ntp_1/g" /etc/ntp.conf /etc/ntp.conf
-sed -i "s/1.centos.pool.ntp.org/$ntp_2/g" /etc/ntp.conf /etc/ntp.conf
-systemctl start ntpd
-timedatectl set-timezone Europe/Sofia
-ntpq -p
-date -R
 
 ## Configure cloudstack repo
 printf "\n############### YUM CONFIG ################\n"
@@ -58,10 +23,41 @@ enabled=1
 gpgcheck=0" > /etc/yum.repos.d/cloudstack.repo
 cat /etc/yum.repos.d/cloudstack.repo
 #gpgkey=http://packages.shapeblue.com/release.asc
+
 ## Configure database
 printf "\n############### DATABASE CONFIG ################\n"
 rpm -ivh mysql-community-release-el7-5.noarch.rpm
-yum install mariadb mariadb-server -y -q
+yum install #!/bin/bash
+## Cloudstack auto-deployment script for Centos 7.
+## place deploy_cloudstack.sh and mysql_secure files in one directory and give them execute rights.
+## Set Vars below and execute ./deploy_cloudstack.sh
+
+## Defining vars
+WORK_DIR="/tmp"
+db_root_password="root"			## mariadb root password
+db_cloud_password="cloud"       ## password for mariadb cloudstack user cloud
+
+## Installing prerequisites
+printf "\n############### PREREQUISITES ################\n"
+yum install expect epel-release -y -q
+yum repolist
+
+## Configure cloudstack repo
+printf "\n############### YUM CONFIG ################\n"
+rpm --import http://packages.shapeblue.com/release.asc
+echo "[cloudstack]
+name=cloudstack
+baseurl=http://packages.shapeblue.com/cloudstack/upstream/centos7/4.14
+enabled=1
+gpgcheck=0" > /etc/yum.repos.d/cloudstack.repo
+cat /etc/yum.repos.d/cloudstack.repo
+#gpgkey=http://packages.shapeblue.com/release.asc
+
+## Configure database
+printf "\n############### DATABASE CONFIG ################\n"
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum install MariaDB-client mariaDB-server -y -q
+yum install mysql-connector-python -y -q
 grep  'binlog-format' /etc/my.cnf 2> /dev/null
 if [ $? == '1' ]; then 
 	sed "/mysql.sock/a innodb_rollback_on_timeout=1\ninnodb_lock_wait_timeout=600\nmax_connections=350\nlog-bin=mysql-bin\nbinlog-format = 'ROW'" /etc/my.cnf
@@ -69,29 +65,18 @@ fi
 systemctl enable mysqld 2>/dev/null
 systemctl start mysqld 2>/dev/null
 systemctl status mysql
-./mysql_secure $db_root_password
-
+/usr/bin/mysql_secure_installation
+#/usr/bin/mysqladmin -u root password 'root'
+#/usr/bin/mysqladmin -u root -h localhost.localdomain password 'root'
 ## Install Cloudstack
 printf "\n############### CLOUDSTACK CONFIG ################\n"
 yum install cloudstack-management -y -q
 alternatives --config java
-cloudstack-setup-databases cloud:$db_cloud_password@localhost --deploy-as=root:$db_root_password
+cloudstack-setup-databases cloud:$db_cloud_password@localhost --deploy-as=root:root
 cloudstack-setup-management
 ### xenserver post install commands
 wget http://download.cloudstack.org/tools/vhd-util -P $WORK_DIR
 yes | cp $WORK_DIR/vhd-util /usr/share/cloudstack-common/scripts/vm/hypervisor/xenserver/vhd-util
-
-
-
-###
-#systemctl disable firewalld 2>/dev/null
-#systemctl stop firewalld 2>/dev/null
-#printf "\n############### FIREWALL CONFIG ################\n"
-#firewall-cmd --zone=public --add-port=8080/tcp --permanent
-#firewall-cmd reload
-#printf "port 8080 tcp enabled"
-#systemctl status firewalld
-
 
 
 
---
+++ CloudStack-Xen/cloudstack-prepOS.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+## Cloudstack OS level settings
+
+## Define Variables
+IP=""
+GATEWAY=""
+NETMASK=""
+DNS1=""
+DNS2=""
+FQDN=""
+NTP1=""
+NTP2=""
+
+## Update system
+printf "\n############### UPDATE SYSTEM ################\n"
+yum upgrade -y
+yum install bridge-utils net-tools -y -q
+
+## Disable SELINUX
+printf "\n############### SELINUX CONFIG ################\n"
+sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+setenforce permissive
+sestatus
+
+## Disable firewalld
+#systemctl disable firewalld 2>/dev/null
+#systemctl stop firewalld 2>/dev/null
+printf "\n############### FIREWALL CONFIG ################\n"
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
+firewall-cmd reload
+printf "port 8080 tcp enabled\n"
+systemctl status firewalld
+
+## Configure FQDN
+printf "\n############### HOSTNAME ################\n"
+printf $FQDN > /etc/hostname
+hostname --fqdn
+
+## Configure ntp
+printf "\n############### NTP CONFIG ################\n"
+yum install ntp -y -q
+sytemctl enable ntpd 2>/dev/null
+sed -i "s/0.centos.pool.ntp.org/$NTP1/g" /etc/ntp.conf /etc/ntp.conf
+sed -i "s/1.centos.pool.ntp.org/$NTP2/g" /etc/ntp.conf /etc/ntp.conf
+systemctl start ntpd
+timedatectl set-timezone Europe/Sofia
+ntpq -p
+date -R
+
+## Configure network
+printf "\n############### NETWORKING CONFIG ################\n"
+echo "DEVICE=cloudbr0
+TYPE=Bridge
+ONBOOT=yes
+BOOTPROTO=static
+IPV6INIT=no
+IPV6_AUTOCONF=no
+DELAY=5
+IPADDR=$IP
+GATEWAY=$GATEWAY
+NETMASK=$NETMASK
+DNS1=$DNS1
+DNS2=$DNS2
+STP=yes
+USERCTL=no
+NM_CONTROLLED=no" > /etc/sysconfig/network-scripts/ifcfg-cloudbr0
+
+echo "TYPE=Ethernet
+BOOTPROTO=none
+DEFROUTE=yes
+NAME=eth0
+DEVICE=eth0
+ONBOOT=yes
+BRIDGE=cloudbr0
+NM_CONTROLLED=no" > /etc/sysconfig/network-scripts/ifcfg-eth0
+
+systemctl enable network
+systemctl restart network
