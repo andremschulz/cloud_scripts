50b651f26948a78b680854bc3a76fd32b20e0856
dredknight <yordank@office.corp>
2021-01-08 17:31:04 +0200

    up

--- VMware/Powercli/compareReports.ps1
+++ VMware/Powercli/compareReports.ps1
@@ -1,3 +1,11 @@
+
+Set-Location -Path C:\scripts\vmware
+
+. ".\code\statics.ps1"
+
+$colours = '#FFEFE6', '#FFFBE1', '#EDFFE2', '#E8F5FF', '#FCD2FF', '#D2FFFC', '#FFD2FA'
+$vCenters = ""
+
 $thisMonth = (Get-Date).ToString('MM')
 $thisYear = (Get-Date).ToString("yyyy")
 $lastMonth = (Get-Date).AddMonths(-1).ToString('MM')
@@ -8,10 +16,6 @@ if ( $lastMonth -eq 12 ) {
 	$LastYear = (Get-Date).ToString("yyyy")
 }
 
-
-
-
-$vCenters = ""
 
 ForEach ($v in $vCenters) {
 	$d11="01/01/2021"
@@ -29,17 +33,42 @@ ForEach ($v in $vCenters) {
 	$pastList = Import-Csv "D:\vmware-output\CSVs\$old_csv"
 	$presentList = Import-Csv "D:\vmware-output\CSVs\$new_csv"
 	$policies = $pastList.tag + $presentList.tag |Sort-Object -Descending |Get-Unique
+	$reportHTML = $(header("$v - policy changes"))
+	$colourCount = 0;
 	ForEach($p in $policies) {
+		$colour   = $colours[$colourCount % $policies.count]
 		#$p="Veeam Backup/APP Aware"
 		$pastVMs = ($pastList | where-Object { $_.tag -eq $p } | Select Name).Name
 		$presentVMs = ($presentList | where-Object { $_.tag -eq $p } | Select Name).Name
 		echo "================================ $p ===================================="
-		$differentVMs = Compare-Object -ReferenceObject @($pastVMs | Select-Object) -DifferenceObject @($presentVMs | Select-Object) #-PassThru
+		$addedVMs = Compare-Object -ReferenceObject @($pastVMs | Select-Object) -DifferenceObject @($presentVMs | Select-Object) | Where-Object {$_.SideIndicator -eq "=>" }
+		$removedVMs = Compare-Object -ReferenceObject @($pastVMs | Select-Object) -DifferenceObject @($presentVMs | Select-Object) | Where-Object {$_.SideIndicator -eq "<=" }
+		$report = @();
+		$vmR  = "" | Select-Object added, removed
+		$i=0
+		while($val -ne 10) {
+			if($addedVMs.InputObject -eq $null -And $removedVMs.InputObject -eq $null) { break }
+			$a = ($addedVMs.InputObject[$i]   | Select-Object )
+			$r = ($removedVMs.InputObject[$i] | Select-Object )
+			if($a -eq $null -And $r -eq $null) { break }
+			
+			$vmR.added   = $a
+			$vmR.removed = $r
+			$report+=$vmR
+			$i++
+		
+		}
+		#$reportHTML 		  +=  $addedVMs.InputObject  |Sort-Object | ConvertTo-Html -PostContent $(body($colourCount)) -PreContent $p
+		#$reportHTML 		  +=  $removedVMs.InputObject|Sort-Object | ConvertTo-Html -PostContent $(body($colourCount)) -PreContent $p
+		$report 	 =  $report | ConvertTo-Html -PostContent $(body($colourCount)) -PreContent $p
+		$reportHTML +=  $report -replace '<table>',"<table class=`"t$colourCount`">"
+		#echo "added  vms in   $P tag - $($report[$p][`"added`"]) "
+		#echo "removed vms from $P tag - $($report[$p][`"removed`"])"
+		$colourCount++
 	}
-	
-
-	$c = Compare-Object -ReferenceObject $pastList.name		-presentListObject $presentList.name -PassThru
-	$c = Compare-Object -ReferenceObject $presentList.name	-presentListObject $pastList.name 	 -PassThru
-	
-	$csv = Import-Csv
+	$outputHTML = "D:\vmware-output\HTMLs\$v`_PolicyCompare_" + (get-date -Format "dd/MM/yyyy/HH/mm") + ".html"
+	ConvertTo-HTML -PostContent "$reportHTML" | Out-File -FilePath "$outputHTML"
 }
+
+
+#Get-Variable $report[$p]["added"] | Select *
