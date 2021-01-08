1468794818eb536cb315a709d705ec6ed4c714c3
dredknight <yordank@office.corp>
2021-01-08 15:28:58 +0200

    up

--- VMware/Powercli/DCsVMsByTag_v2.ps1
+++ VMware/Powercli/DCsVMsByTag_v2.ps1
@@ -16,12 +16,7 @@ Param(
 
 . ".\code\statics.ps1"
 
-$vCenters =
-"ilhq-bk-vci1.office.corp",
-"dedc-bk-vci1.office.corp",
-"dedc-bk-vcd1.office.corp",
-"bghq-bk-vci01.office.corp",
-"bgdc-bk-vci1.office.corp"
+$vCenters = ""
 
 $colours = '#FFEFE6', '#FFFBE1', '#EDFFE2', '#E8F5FF', '#FCD2FF', '#D2FFFC', '#FFD2FA'
 
@@ -29,73 +24,92 @@ $reportList = @()
  ForEach ($v in $vCenters)
  {
 	$report = ""
-	$outputHTML = "D:\vmware-output\$v`_VMsByTag_" + (get-date -Format "dd/MM/yyyy/HH/mm") + ".html"
-	$outputCSV = "D:\vmware-output\$v`_VMsByTag_" + (get-date -Format "dd/MM/yyyy/HH/mm") + ".csv"
+	$outputHTML = "D:\vmware-output\HTMLs\$v`_VMsByTag_" + (get-date -Format "dd/MM/yyyy/HH/mm") + ".html"
+	$outputCSV = "D:\vmware-output\CSVs\$v`_VMsByTag_" + (get-date -Format "dd/MM/yyyy/HH/mm") + ".csv"
 	
 	if( (login $v $u $p) -eq 0) {
 		$report=""
 		$vmList=@()
 		$vmsByTag=@{}
+		echo "Collecting data..."
 		$vms = Get-VM -Location (Get-Datacenter -Server $v).Name
+		
+		
+		echo "Begining data mapping..."
+		$ListSize = $vms.Length
+		$VMCounter = 1
+		$PrintMark = 10
+		
 		foreach($vm in $vms){
-		   
-		   $this = "" | Select-Object name, uuid, hostname, os, state, vcpu, cores, memoryGB, host, datastore, totalSizeGB, nics, tags, IPs, notes, version, iso
-		   $this.name 				= $vm.Name
-		   #$this.uuid 				= $vm.ExtensionData.Config.Uuid
-		   #$this.hostname 			= $vm.ExtensionData.Summary.Guest.HostName
-		   $this.os 				= $vm.ExtensionData.Config.GuestFullName
-		   $this.state 			= $vm.PowerState
-		   #$this.vCPU	 			= $vm.NumCpu 			#$vm.summary.config.numcpu
-		   #$this.cores	 			= $vm.CoresPerSocket 	#$vm.summary.config.numcpu
-		   #$this.memoryGB	 		= $vm.MemoryGB
-		   $this.host			    = $vm.VMHost.Name
-		   #$this.notes 				= $vm.Notes
-		   $this.version 			= $vm.ExtensionData.Config.Version
-		   if (($vm |Get-CDDrive).IsoPath)	{ $iso = "yes" } else { $iso = "" }
-		   $this.iso 				= $iso
-		   #$this.nics 				= $vm.ExtensionData.Summary.Config.NumEthernetCards #$vm.Summary.Config.NumEthernetCards
-		   #$this.IPs 				= $vm.Guest.IPAddress -join '|'
-		   $this.tags 				= (Get-TagAssignment -Entity $vm).Tag
-		   $this.totalSizeGB 		= [System.Math]::Round($vm.UsedSpaceGB)
-		   #$this.usedSpaceGB 		= [math]::Round($vm.Summary.Storage.Committed/1GB,2)
-		   #$this.provisionedSpaceGB = [math]::Round($vm.Summary.Storage.UnCommitted/1GB,2)
-		   #$this.datastore 			= $vm.Config.DatastoreUrl[0].Name
-		   $vmList+=$this
+			$this = "" | Select-Object name, uuid, hostname, os, state, vcpu, cores, memoryGB, host, datastore, totalSizeGB, nics, tags, IPs, notes, version, iso, tag
+			$this.name 				= $vm.Name
+			#$this.uuid 				= $vm.ExtensionData.Config.Uuid
+			#$this.hostname 			= $vm.ExtensionData.Summary.Guest.HostName
+			$this.os 				= $vm.ExtensionData.Config.GuestFullName
+			$this.state 			= $vm.PowerState
+			#$this.vCPU	 			= $vm.NumCpu 			#$vm.summary.config.numcpu
+			#$this.cores	 			= $vm.CoresPerSocket 	#$vm.summary.config.numcpu
+			#$this.memoryGB	 		= $vm.MemoryGB
+			$this.host				 = $vm.VMHost.Name
+			#$this.notes 				= $vm.Notes
+			$this.version 			= $vm.ExtensionData.Config.Version
+			if (($vm |Get-CDDrive).IsoPath)	{ $iso = "yes" } else { $iso = "" }
+			$this.iso 				= $iso
+			#$this.nics 				= $vm.ExtensionData.Summary.Config.NumEthernetCards #$vm.Summary.Config.NumEthernetCards
+			#$this.IPs 				= $vm.Guest.IPAddress -join '|'
+			$this.tags 				= (Get-TagAssignment -Entity $vm).Tag
+			$this.totalSizeGB 		= [System.Math]::Round($vm.UsedSpaceGB)
+			#$this.usedSpaceGB 		= [math]::Round($vm.Summary.Storage.Committed/1GB,2)
+			#$this.provisionedSpaceGB = [math]::Round($vm.Summary.Storage.UnCommitted/1GB,2)
+			#$this.datastore 			= $vm.Config.DatastoreUrl[0].Name
+			$vmList+=$this
+			#echo "$($VMCounter/$ListSize)"
+			if ("$(($VMCounter/$ListSize)*100)" > $PrintMark) {
+				echo "$PrintMark%..."
+				$PrintMark+=10;
+			}
+		   $VMCounter++;
 		}
 		#$vmList| where  {$_.name -eq "ILHQ-VMM01"} |select Name, Tags| Format-Table
-		$vmsByTag["VMs without tag"]=@()
+		echo "Policy filtering..."
+		$vmsByTag["no tag"]=@()
 		foreach($vm in $vmList){
 			$flag = 0
 			foreach($tag in $vm.tags){
 				if($tag.Category.Name -eq $c) {
 					if( $vmsByTag["$tag"] -isnot [array]) { $vmsByTag["$tag"] = @() }
+					$vm.tag = $tag;
 					$vmsByTag["$tag"]+=$vm;
 					$flag = 1;
 				}
 			}
-			if($flag -eq 0) { $vmsByTag["VMs without tag"]+=$vm }
+			if($flag -eq 0) { 
+				$vm.tag = "no tag";
+				$vmsByTag["no tag"]+=$vm 
+			}
 		}
 		
 		### Sorting data and preparing the report
 		$colourCount = 0;
 		
-
-		$report = $header
+		echo "Generating reports..."
+		$report = $(header("$v - VMs by TAG"))
+		$csv = @();
 		foreach($key in $vmsByTag.keys){
 			$colour   = $colours[$colourCount % $vmsByTag.keys.count]
-			$fragment = $vmsByTag["$key"] |Sort-Object -Property Name| Select name, state, host, totalSizeGB, version, os, iso
-			$fragment | export-csv $outputCSV
-
-			$html 	  = $fragment| ConvertTo-Html -Head $(header("$v - VMs by TAG")) -PostContent $(body($colourCount)) -PreContent $key
+			$fragment = $vmsByTag["$key"] 
+			$csv	 += $fragment| Sort-Object -Property Name| Select name, state, host, totalSizeGB, version, os, iso, tag
+			$html 	  = $fragment| Sort-Object -Property Name| Select name, state, host, totalSizeGB, version, os, iso| ConvertTo-Html -PostContent $(body($colourCount)) -PreContent $key
 			$report  += $html  -replace '<table>',"<table class=`"t$colourCount`">"
 			$colourCount++
 		}
 		ConvertTo-HTML -PostContent "$report" | Out-File -FilePath "$outputHTML"
+		$csv |export-csv $outputCSV
 		
 		logout($v)
 		$reportList = $reportList + $outputHTML
 	}
 }
-
+echo "Sending mails..."
 if($em -eq "") { email -Body "VMs by backup tag for each vcenter in Black environment" -Subject "Global Black: VMs by Backup tag" -attachment $reportList }
 else { email -emailTo $em -Body "VMs by backup tag for each vcenter in Black environment" -Subject "Global Black: VMs by Backup tag" -attachment $reportList }
--- VMware/Powercli/compareReports.ps1
+++ VMware/Powercli/compareReports.ps1
@@ -1,5 +1,3 @@
-(Get-ChildItem -Path D:\vmware-output -Filter *.html | Sort-Object -Property CreationTime -Descen |select Name, CreationTime |Format-Table).CreationTime.month
-
 $thisMonth = (Get-Date).ToString('MM')
 $thisYear = (Get-Date).ToString("yyyy")
 $lastMonth = (Get-Date).AddMonths(-1).ToString('MM')
@@ -12,9 +10,36 @@ if ( $lastMonth -eq 12 ) {
 
 
 
-$last_files = Get-ChildItem -Path D:\vmware-output -Filter *.html | where-Object { $_.CreationTime -ge "$lastMonth/01/$LastYear" -and $_.CreationTime -le "$lastMonth/31/$LastYear"}
-$new_files = Get-ChildItem -Path D:\vmware-output -Filter *.html | where-Object { $_.CreationTime -ge "$thisMonth/01/$thisYear" -and $_.CreationTime -le "$thisMonth/31/$thisYear"}
-$old_csv = $last_files[-1]
-$new_csv = $new_files[-1]
-$old_csv
-$new_csv
+
+$vCenters = ""
+
+ForEach ($v in $vCenters) {
+	$d11="01/01/2021"
+	$d12="01/08/2021"
+	$d21="01/08/2021"
+	$d22="01/30/2021"
+	#$pastFiles = Get-ChildItem -Path D:\vmware-output\CSVs -Filter *.csv | where-Object { $_.CreationTime -ge "$lastMonth/01/$LastYear" -and $_.CreationTime -le "$lastMonth/31/$LastYear"}
+	$pastFiles = Get-ChildItem -Path D:\vmware-output\CSVs -Filter "$source*.csv" | where-Object { $_.CreationTime -ge "$d11" -and $_.CreationTime -le "$d12"}
+	#$presentFiles = Get-ChildItem -Path D:\vmware-output\CSVs -Filter *.csv | where-Object { $_.CreationTime -ge "$thisMonth/01/$thisYear" -and $_.CreationTime -le "$thisMonth/31/$thisYear"}
+	$presentFiles = Get-ChildItem -Path D:\vmware-output\CSVs -Filter "$source*.csv" | where-Object { $_.CreationTime -ge "$d21" -and $_.CreationTime -le "$d22"}
+	$old_csv = $pastFiles[-1]
+	$new_csv = $presentFiles[-1]
+	$old_csv
+	$new_csv
+	$pastList = Import-Csv "D:\vmware-output\CSVs\$old_csv"
+	$presentList = Import-Csv "D:\vmware-output\CSVs\$new_csv"
+	$policies = $pastList.tag + $presentList.tag |Sort-Object -Descending |Get-Unique
+	ForEach($p in $policies) {
+		#$p="Veeam Backup/APP Aware"
+		$pastVMs = ($pastList | where-Object { $_.tag -eq $p } | Select Name).Name
+		$presentVMs = ($presentList | where-Object { $_.tag -eq $p } | Select Name).Name
+		echo "================================ $p ===================================="
+		$differentVMs = Compare-Object -ReferenceObject @($pastVMs | Select-Object) -DifferenceObject @($presentVMs | Select-Object) #-PassThru
+	}
+	
+
+	$c = Compare-Object -ReferenceObject $pastList.name		-presentListObject $presentList.name -PassThru
+	$c = Compare-Object -ReferenceObject $presentList.name	-presentListObject $pastList.name 	 -PassThru
+	
+	$csv = Import-Csv
+}
